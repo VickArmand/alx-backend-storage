d79ace8f5e904bfc5b01fb13d891b70e18e79756
VickArmand <victormaina1962@gmail.com>
2024-04-28 15:44:32 +0300

    corrections v1

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -3,4 +3,4 @@
 -- Requirements:
 -- Import this table dump: names.sql.zip
 -- Only the first letter of name must be indexed
-CREATE INDEX idx_name_first ON names(name);
+CREATE INDEX idx_name_first ON names(name(1));
