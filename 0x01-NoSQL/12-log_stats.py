6122001e93d6e3422041fef53e61cb63d3af57b0
VickArmand <victormaina1962@gmail.com>
2024-05-01 19:48:11 +0300

    pycodestyle

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,7 +6,8 @@ about Nginx logs stored in MongoDB:
 Database: logs, Collection: nginx
 first line: x logs where x is the number of documents in this collection
 second line: Methods:
-5 lines with the number of documents with the method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order (see example below - warning: it’s a tabulation before each line)
+5 lines with the number of documents with the
+method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order
 one line with the number of documents with:
 method=GET
 path=/status
@@ -21,5 +22,7 @@ methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 print("{} logs".format(collection.count_documents({})))
 print("Methods:")
 for method in methods:
-    print("\tmethod {}: {}".format(method, collection.count_documents({"method" : method})))
-print("{} status check".format(collection.count_documents({"method": "GET", "path": "/status"})))
+    print("\tmethod {}: {}".format(method, collection.count_documents(
+        {"method": method})))
+print("{} status check".format(collection.count_documents(
+    {"method": "GET", "path": "/status"})))
