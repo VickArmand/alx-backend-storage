43421bb2cb65cc596ea0e3b7cfdac4336a0fc750
VickArmand <victormaina1962@gmail.com>
2024-05-02 03:45:07 +0300

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""This module has a class Cache"""
+import redis
+import uuid
+
+
+class Cache:
+    """This class has a constructor and a store method"""
+    def __init__(self):
+        """
+        store an instance of the Redis client as a private variable
+        named _redis (using redis.Redis()) and
+        flush the instance using flushdb.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data):
+        """
+        takes a data argument and returns a string.
+        The method should generate a random key (e.g. using uuid),
+        store the input data in Redis using the random key and return the key.
+        """
+        self.id = str(uuid.uuid4())
+        self._redis.set(self.id, data)
+        return self.id
