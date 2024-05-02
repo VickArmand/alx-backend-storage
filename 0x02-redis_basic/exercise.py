487fa7fd4abcb37bf6ad9671506c2831fc2b4200
VickArmand <victormaina1962@gmail.com>
2024-05-02 05:31:55 +0300

    storing retrieving lists and implementing web cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,60 @@
 """This module has a class Cache"""
 import redis
 import uuid
+from functools import wraps
 from typing import Union, Callable, Optional
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    decorates a method to record its input output history
+    """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        meth_name = method.__qualname__
+        self._redis.rpush(meth_name + ":inputs", str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(meth_name + ":outputs", output)
+        return output
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """
+    displays the history of calls made by a particular method by retrieving
+    the inputs and outputs saved on the redis store
+    """
+    meth_name = method.__qualname__
+    redis_db = method.__self__._redis
+    inputs = redis_db.lrange(meth_name + ":inputs", 0, -1)
+    outputs = redis_db.lrange(meth_name + ":outputs", 0, -1)
+
+    print(f"{meth_name} was called {len(inputs)} times:")
+    for input, output in zip(inputs, outputs):
+        input = input.decode("utf-8")
+        output = output.decode("utf-8")
+        print(f"{meth_name}(*{input}) -> {output}")
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    decorates a method to count how many times it was called
+    """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -27,7 +80,11 @@ class Cache:
         return self.id
 
     def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+            fn: Optional[Callable] = None) -> Union[str,
+                                                    bytes,
+                                                    int,
+                                                    float,
+                                                    None]:
         """
         take a key string argument and
         an optional Callable argument named fn.
