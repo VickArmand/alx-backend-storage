bc0ff72e87573a1fcc13b3a182c9909bf79e42ed
VickArmand <victormaina1962@gmail.com>
2024-05-05 02:52:04 +0300

    Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+The core of the function is very simple.
+It uses the requests module to obtain the HTML content
+of a particular URL and returns it.
+"""
+import redis
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    """
+    track how many times a particular URL was accessed in the key "count:{url}"
+    and cache the result with an expiration time of 10 seconds.
+    Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow response
+    and test your caching.
+    Bonus: implement this use case with decorators.
+    """
+    import requests as re
+    return re.get(url).text
---
+++ 0x02-redis_basic/webtest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from web import get_page
+
+get_page('http://slowwly.robertomurray.co.uk')
