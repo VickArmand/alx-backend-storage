055a9497293f2da4ab8950572b51fe6006b0a4f8
VickArmand <victormaina1962@gmail.com>
2024-05-04 17:18:09 +0300

    Top students

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+First restore backup into database using mongorestore dump
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """
+    a Python script that provides some stats
+    about Nginx logs stored in MongoDB:
+    Database: logs, Collection: nginx
+    first line: x logs where
+    x is the number of documents in this collection
+    second line: Methods:
+    5 lines with the number of documents with the
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"] in this order
+    one line with the number of documents with:
+    method=GET
+    path=/status
+    """
+    client = MongoClient(host="localhost", port=27017)
+    collection = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    for method in methods:
+        print("\tmethod {}: {}".format(method, collection.count_documents(
+            {"method": method})))
+    print("{} status check".format(collection.count_documents(
+        {"method": "GET", "path": "/status"})))
