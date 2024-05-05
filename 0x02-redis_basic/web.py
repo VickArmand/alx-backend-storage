1d77d9f619a2e54b813665ebb70c7ddd22a6a496
VickArmand <victormaina1962@gmail.com>
2024-05-05 15:24:14 +0300

    final

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""
+The core of the function is very simple.
+It uses the requests module to obtain the HTML content
+of a particular URL and returns it.
+"""
 import redis
-import requests
+import requests as re
 from functools import wraps
 from typing import Callable
 
 
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
 
 
 def data_cacher(method: Callable) -> Callable:
@@ -32,7 +33,11 @@ def data_cacher(method: Callable) -> Callable:
 
 @data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """
+    track how many times a particular URL was accessed in the key "count:{url}"
+    and cache the result with an expiration time of 10 seconds.
+    Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow response
+    and test your caching.
+    Bonus: implement this use case with decorators.
+    """
+    return re.get(url).text
